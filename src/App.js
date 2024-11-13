21a8f3964f9a4c450736c7f667b6960d2c287d00
Daniel Numair <daniel@raldts.com.br>
2024-11-12 23:34:18 -0300

    commit da app

--- src/App.js
+++ src/App.js
@@ -4,20 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello World!</h1>
+      <h2>Primeira Atividade Prática AP1</h2>
+      <p>Desenvolvimento de Aplicações Orientada a Componentes</p>
     </div>
   );
 }
